c450e87ac7e25e75fcab26bf2047b94d83783efe
famalkhadija <famalkhadija@gmail.com>
2026-04-21 13:28:16 +0500

    pdh

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,5 +1,5 @@
 import { Resend } from 'resend';
-// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST(req) {
   try {
     const body = await req.json();
