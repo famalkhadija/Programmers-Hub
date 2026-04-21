0e0b787b27904d6eb55c4834b1b4e4d492b00c19
famalkhadija <famalkhadija@gmail.com>
2026-04-21 15:30:40 +0500

    pdh

--- app/layout.js
+++ app/layout.js
@@ -1,27 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Poppins } from 'next/font/google';
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ThemeProvider from "@/components/ThemeWrapper";
 import SessionWrapper from "@/components/SessionWrapper";
 import ClientWrapper from '@/components/ClientWrapper';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['600'], // Choose weights you need
-  variable: '--font-poppins',
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Programmers Discussion Hub",
