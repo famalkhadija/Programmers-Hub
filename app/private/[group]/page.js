dd96861a03825978fc2dc4d02403801d9c596b69
famalkhadija <famalkhadija@gmail.com>
2026-05-16 19:57:52 +0500

    finalTask

--- app/private/[group]/page.js
+++ app/private/[group]/page.js
@@ -1,18 +1,22 @@
 import PrivateDiscussClient from "@/components/PrivateDiscussClient";
+
 export async function generateMetadata({ params }) {
   const rawGroup = params?.group || "";
 
-  const groupName = rawGroup
+  // email remove
+  const groupOnly = rawGroup.split("_").slice(1).join(" ");
+
+  const groupName = decodeURIComponent(groupOnly)
     .replace(/plus/g, "+")
     .replace(/-/g, " ")
     .replace(/\b\w/g, (l) => l.toUpperCase());
 
   return {
-    title: `${groupName} Discussion`,
+    title: `${groupName} | Programmers Discussion Hub`,
     description: `Discuss ${groupName} with other members`,
   };
 }
 
 export default function GroupPage() {
- return <PrivateDiscussClient />;
+  return <PrivateDiscussClient />;
 }
